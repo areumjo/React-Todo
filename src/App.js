e663682d264ed88b606f511257b0b14ff61a467e
Areum Jo <areumjo1@gmail.com>
2019-08-05 15:20:37 -0400

    Added example todo list and map it over

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ class App extends React.Component {
   constructor() {
     super();
     this.state={
-      todo: ['Learn React']
+      todo: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
     }
   }
   
@@ -16,7 +27,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        {this.state.todo}
+        {this.state.todo.map(e => <p>{e.task}</p>)}
         <TodoForm />
       </div>
     );
